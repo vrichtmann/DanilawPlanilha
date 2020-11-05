94aa9106cc54889436bc49c4068bb987c27bd30a
vrichtmann <vrichtmann@gmail.com>
2020-11-05 20:48:35 -0300

    Get Test Json Info

--- board/boardContent.js
+++ board/boardContent.js
@@ -1,15 +1,17 @@
 
 var BoardContent = function(){
 
+    var testeJson = [{name:"Barnard Posselt", initTime: "4", finalTime: "11", color:"#7d3f80"}];
+
     this.createBoardContent = function(){
         var boardContentTXT = "";
 
         boardContentTXT += "<div class='gantt__row'>";
         boardContentTXT += "       <div class='gantt__row-first'>"
-        boardContentTXT += "         Barnard Posselt"
+        boardContentTXT += testeJson[0]["name"];
         boardContentTXT += "        </div>"
         boardContentTXT += "      <ul class='gantt__row-bars'>"
-        boardContentTXT += "          <li style='grid-column: 4/11; background-color: #2ecaac;'>Even longer project</li>"
+        boardContentTXT += "          <li style='grid-column: " + testeJson[0]["initTime"] +"/" + testeJson[0]["finalTime"] + "; background-color: " + testeJson[0]["color"] + ";'>Even longer project</li>"
         boardContentTXT += "      </ul>"
         boardContentTXT += "    </div>"
 
--- board/boardTitle.js
+++ board/boardTitle.js
@@ -5,7 +5,7 @@ var BoardTitle = function(){
     this.createHeaderTitle = function(){
         var boardHeadertitle = "";
         boardHeadertitle += "<div class='header'>";
-        boardHeadertitle += "   <h2>Gantt Chart Concept with CSS-Grid</h2>";
+        boardHeadertitle += "   <h2>Projeto Organograma Daniel Meireles de Formica</h2>";
         boardHeadertitle += "   <p>Use <strong>grid-column</strong> to span a new entry across the grid. (e. g. <strong>grid-column: 5/12;</strong> spans an entry from May to November)</p>"
         boardHeadertitle += " </div>"
     
